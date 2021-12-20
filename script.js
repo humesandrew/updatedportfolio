e8a03c0b88bc4c1f8a57130b9539910080de3386
Andy Humes <andrew.humes@du.edu>
2021-12-20 16:57:48 -0700

    got github api element to display

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 const bioCard = document.getElementById("card1");
 bioCard.onclick = function displayBioCard() {
-  bioCard.style.backgroundImage = "none";
-  bioCard.style.color = "black";
+//   bioCard.style.backgroundImage = "none";
+  bioCard.style.color = "white";
   bioCard.style.fontSize = "30px";
   bioCard.textContent =
     "I am an aspiring coder. On this page, you will see some examples of my work, my resume, and contact info. I am also part of the leadership for the Colorado chapter of the Foundation Fighting Blindness.";
@@ -39,6 +39,9 @@ projectCard.onclick = function displayProjectCard() {
     var projectCardApiEl = document.createElement("card");
     projectCardApiEl.setAttribute("id", "projectCardApiEl");
     projectCardApiEl.setAttribute("class", "card col-10");
+    projectCardApiEl.textContent = "Click the link to see the deployed project.";
+    projectCardApiEl.style.color = "black";
+    projectCardApiEl.style.fontSize = "20px";
     projectCard.appendChild(projectCardApiEl);
     
 
@@ -46,10 +49,63 @@ projectCard.onclick = function displayProjectCard() {
     projectCardApiEl.appendChild(createProjectButton);
     createProjectButton.textContent = "BellyBuster: the App";
     createProjectButton.setAttribute("class", "btn btn-outline-secondary btn-lg");
-
-    projectCard.style.justifyContent = "space-around";
-    projectCard.style.alignItems = "row";
+    createProjectButton.setAttribute("id", "projectButton");
+    var clickProjectButton = document.getElementById("projectButton");
+    clickProjectButton.onclick = function () {
+      window.open("https://humesandrew.github.io/Belly-Buster/");
     }
+
+    var createApiCard = document.createElement("div");
+    createApiCard.textContent = "hello";
+    createApiCard.setAttribute("id", "apiCard");
+    projectCardApiEl.appendChild(createApiCard);
+
+    
+    function getRepos() {
+      var apiUrl = 'https://api.github.com/users/humesandrew/repos?per_page=5';
+      fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+            
+          for (var i = 0; i < data.length; i++) {
+            var repoName = data[i].name;
+            var repoUrl = data[i].html_url;
+  
+          
+
+                var repoEl = document.createElement('button');
+                repoEl.textContent = repoName;
+                repoEl.setAttribute("id", repoName);
+                repoEl.setAttribute("url", repoUrl);
+              repoEl.setAttribute('class', 'btn btn-outline-secondary btn-block');
+            
+              
+               projectCardApiEl.appendChild(repoEl);
+            
+          }
+
+
+
+
+          });
+
+          
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+  }; 
+
+  getRepos();
+    }
+
+
+
 
 
 
@@ -83,20 +139,17 @@ contactCard.onclick = function displayContactCard() {
 
     var clickEmail = document.getElementById("Email");
     clickEmail.onclick = function () {
-      console.log("Email");
       window.location.href = "mailto:humes.andrew@gmail.com";
     };
 
     var clickResume = document.getElementById("Resume");
     clickResume.onclick = function () {
-      console.log("Resume");
       window.open("Andy_Humes_Resume.pdf");
 
     };
 
     var clickLinkedIn = document.getElementById("LinkedIn");
     clickLinkedIn.onclick = function () {
-      console.log("LinkedIn");
       window.open("https://www.linkedin.com/in/andrew-humes-72075559/");
     };
   }

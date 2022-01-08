612faad5945258ed3462d714ecfe60ef210e8ca3
Andy Humes <andrew.humes@du.edu>
2022-01-08 16:37:17 -0700

    finished based on HW feedback

---
+++ readme.md
@@ -0,0 +1,8 @@
+Description: this is an updated portfolio of my work. The goal of the page is to show my coding capability, to reference examples of my work, and to also act as my contact page for the Foundation Fighting Blindness. The deployed application covers all of the requirements requested. The page is adaptive, as it adjusts the display based on your screen size. The page is interactive, in that clicking elements either changes the dispay or sends you to the link. 
+
+Usage: 
+![Screenshot](pagethumb.png)
+
+
+Credits: 
+I used bootstrap for the framework. I used images from my publication in EyeNet Magazine, as well as the logo for the Foundation Fighting Blindness. 
--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const bioCard = document.getElementById("card1");
 bioCard.onclick = function displayBioCard() {
-//   bioCard.style.backgroundImage = "none";
+  //   bioCard.style.backgroundImage = "none";
   bioCard.style.color = "white";
   bioCard.style.fontSize = "30px";
   bioCard.textContent =
@@ -26,105 +26,104 @@ const projectCard = document.getElementById("card2");
 const createProjectBreak = document.createElement("hr");
 
 projectCard.onclick = function displayProjectCard() {
-    projectCard.textContent = '';
-    var projectCardEl = document.createElement("card");
-    projectCardEl.setAttribute("id", "projectCardEl");
+  projectCard.textContent = "";
+  var projectCardEl = document.createElement("card");
+  projectCardEl.setAttribute("id", "projectCardEl");
+
+  projectCardEl.setAttribute("class", "card-header");
+  var projectText = document.getElementById("projectCardEl");
+  projectCardEl.textContent = "Projects";
+
+  projectCard.appendChild(projectCardEl);
+
+  var projectCardApiEl = document.createElement("card");
+  projectCardApiEl.setAttribute("id", "projectCardApiEl");
+  projectCardApiEl.setAttribute("class", "card col-10");
+  projectCardApiEl.textContent = "";
+  projectCardApiEl.style.backgroundColor = "transparent";
+  projectCard.appendChild(projectCardApiEl);
+  
+
+  var createProjectButton = document.createElement("button");
+  projectCardApiEl.appendChild(createProjectButton);
+  createProjectButton.textContent = "BellyBuster: the App";
+  createProjectButton.setAttribute(
+    "class",
+    "btn btn-secondary btn-lg btn-light"
+  );
+  createProjectButton.setAttribute("id", "projectButton");
+  createProjectButton.setAttribute("style", "margin-top: 20px");
+  createProjectButton.setAttribute("style", "background-image: url('bellybusterthumb.png')", "background-size: auto");
+  createProjectButton.style.fontWeight = "bold";
+  // createProjectButton.setAttribute("style", "background-size: cover");
+  
+  var createProjectButtonLinks = document.createElement("div");
+  createProjectButtonLinks.textContent = "GitHub Url: " + "https://github.com/humesandrew/Belly-Buster";
+              createProjectButtonLinks.setAttribute("style", "font-size: 10px");
 
 
-    projectCardEl.setAttribute("class", "card-header");
-    var projectText = document.getElementById("projectCardEl");
-   projectCardEl.textContent = "Projects";
-    
-    projectCard.appendChild(projectCardEl);
+  createProjectButton.appendChild(createProjectBreak).appendChild(createProjectButtonLinks);
+  var clickProjectButton = document.getElementById("projectButton");
+  clickProjectButton.onclick = function () {
+    window.open("https://humesandrew.github.io/Belly-Buster/");
+  };
 
+  var createApiCard = document.createElement("div");
+  createApiCard.textContent = "";
+  createApiCard.setAttribute("id", "apiCard");
+  createApiCard.setAttribute("style", "margin-top: 30px;");
+  projectCardApiEl.appendChild(createApiCard);
 
-    var projectCardApiEl = document.createElement("card");
-    projectCardApiEl.setAttribute("id", "projectCardApiEl");
-    projectCardApiEl.setAttribute("class", "card col-10");
-    projectCardApiEl.textContent = "";
-    projectCardApiEl.style.color = "black";
-    projectCardApiEl.style.fontSize = "20px";
-    projectCardApiEl.style.backgroundColor = "transparent";
-    projectCard.appendChild(projectCardApiEl);
-    
-
-    var createProjectButton = document.createElement("button");
-    projectCardApiEl.appendChild(createProjectButton);
-    createProjectButton.textContent = "BellyBuster: the App";
-    createProjectButton.setAttribute("class", "btn btn-secondary btn-lg btn-light");
-    createProjectButton.setAttribute("id", "projectButton");
-    createProjectButton.setAttribute("style", "margin-top: 20px");
-    // var createProjectBreak = document.createElement("hr");
-    createProjectButton.appendChild(createProjectBreak);
-    var clickProjectButton = document.getElementById("projectButton");
-    clickProjectButton.onclick = function () {
-      window.open("https://humesandrew.github.io/Belly-Buster/");
-    }
-
-    var createApiCard = document.createElement("div");
-    createApiCard.textContent = "";
-    createApiCard.setAttribute("id", "apiCard");
-    createApiCard.setAttribute("style", "margin-top: 30px;");
-    projectCardApiEl.appendChild(createApiCard);
-
-    
-    function getRepos() {
-      var apiUrl = 'https://api.github.com/users/humesandrew/repos?per_page=5';
-      fetch(apiUrl)
+  function getRepos() {
+    var apiUrl = "https://api.github.com/users/humesandrew/repos?per_page=5";
+    fetch(apiUrl)
       .then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
             console.log(data);
-            
-          for (var i = 0; i < data.length; i++) {
-            var repoName = data[i].name;
-            var repoUrl = data[i].html_url;
-  
-            
-          
 
-                var repoEl = document.createElement('a');
-                repoEl.textContent = repoName;
-                repoEl.setAttribute("id", repoName);
-               
-                // repoEl.setAttribute("url", repoUrl);//
-              repoEl.setAttribute('class', 'btn btn-secondary btn-block');
-            repoEl.setAttribute('href', repoUrl);
+            for (var i = 0; i < data.length; i++) {
+              var repoName = "Title: " + data[i].name;
+              var repoUrl = "https://humesandrew.github.io/" + data[i].name;
+              var addRepoSection = document.createElement("hr");
+              var addRepoSectionText = document.createElement("div");
+              addRepoSectionText.textContent = "GitHub Url: " + repoUrl;
+              addRepoSectionText.setAttribute("style", "font-size: 10px");
+              addRepoSectionText.style.color = 'black';
               
-               projectCardApiEl.appendChild(repoEl);
+              
+
+              var repoEl = document.createElement("a");
+              repoEl.textContent = repoName;
+              repoEl.setAttribute("id", repoName);
+
+              repoEl.setAttribute("class", "btn btn-secondary btn-block");
+              repoEl.setAttribute("href", repoUrl);
+              repoEl.setAttribute("style", "display: flex");
+              repoEl.setAttribute("style", "justify-content: center");
+              repoEl.setAttribute("style", "align-items: column");
+              repoEl.setAttribute("style", "background-image: url('githubthumb.png')");
+              repoEl.style.color = 'black';
+              repoEl.style.fontWeight = 'bold';
+
             
-          }
 
+              projectCardApiEl.appendChild(repoEl).appendChild(addRepoSection).appendChild(addRepoSectionText);
+            }
 
-
-
+            
           });
-
-          
         } else {
-          alert('Error: ' + response.statusText);
+          alert("Error: " + response.statusText);
         }
       })
       .catch(function (error) {
-        alert('Unable to connect to GitHub');
+        alert("Unable to connect to GitHub");
       });
-  }; 
+  }
 
   getRepos();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 ///creating the contact card on click////////
 
@@ -137,28 +136,24 @@ contactCard.onclick = function displayContactCard() {
   for (var i = 0; i < contacts.length; i++) {
     var createListItem = document.createElement("button");
     createListItem.textContent = contacts[i];
+    createListItem.setAttribute("class", "btn btn-secondary btn-lg btn-light");
     contactCard.appendChild(createListItem);
-
     contactCard.style.justifyContent = "space-around";
-    createListItem.style.borderRadius = "10px";
-    createListItem.style.fontSize = "25px";
-
     createListItem.setAttribute("id", contacts[i]);
   }
 
-    var clickEmail = document.getElementById("Email");
-    clickEmail.onclick = function () {
-      window.location.href = "mailto:humes.andrew@gmail.com";
-    };
+  var clickEmail = document.getElementById("Email");
+  clickEmail.onclick = function () {
+    window.location.href = "mailto:humes.andrew@gmail.com";
+  };
 
-    var clickResume = document.getElementById("Resume");
-    clickResume.onclick = function () {
-      window.open("Andy_Humes_Resume.pdf");
+  var clickResume = document.getElementById("Resume");
+  clickResume.onclick = function () {
+    window.open("Andy_Humes_Resume.pdf");
+  };
 
-    };
-
-    var clickLinkedIn = document.getElementById("LinkedIn");
-    clickLinkedIn.onclick = function () {
-      window.open("https://www.linkedin.com/in/andrew-humes-72075559/");
-    };
-  }
+  var clickLinkedIn = document.getElementById("LinkedIn");
+  clickLinkedIn.onclick = function () {
+    window.open("https://www.linkedin.com/in/andrew-humes-72075559/");
+  };
+};
